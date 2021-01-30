52081def94bda10ad49c96d869d5d001b950de71
Darya_Kotova <darya_kotova@epam.com>
2021-01-30 22:11:25 +0600

    Add project files.

---
+++ OddEven.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OddEven.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("OddEven.Tests")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d504060f-0592-4248-8147-d911af5ed0e3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ OddEven.Tests/TestsOddEven.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OddEven.Tests
+{
+    [TestClass]
+    public class TestsOddEven
+    {
+        [TestMethod]
+         public void CanPrintFrom1To50()
+        {
+            OddEvenClass oddEven = new OddEvenClass();
+            int startRange = 1;
+            int endRange = 50;
+            var result = oddEven.PrintOddEven(startRange, endRange);
+            Assert.IsNotNull(result, string.Format("{0}", result));
+        }
+
+        [TestMethod]
+        public void CanPrintFrom1To100()
+        {
+            OddEvenClass oddEven = new OddEvenClass();
+            int startRange = 1;
+            int endRange = 100;
+            var result = oddEven.PrintOddEven(startRange, endRange);
+            Assert.IsNotNull(result, string.Format("{0}", result));
+        }
+
+        [TestMethod]
+        public void CanPrintOddEvenForSingleNumber1()
+        {
+            OddEvenClass oddEven = new OddEvenClass();
+            int number = 1;
+            var actualresult = oddEven.PrintSingleOddEven(number);
+            var expectedresult = "Odd";
+            Assert.AreEqual(expectedresult, actualresult);
+        }
+        [TestMethod]
+        public void CanPrintOddEvenForSingleNumber3()
+        {
+            OddEvenClass oddEven = new OddEvenClass();
+            int number = 3;
+            var actualresult = oddEven.PrintSingleOddEven(number);
+            var expectedresult = "3";
+            Assert.AreEqual(expectedresult, actualresult);
+        }
+
+        [TestMethod]
+        public void CanPrintOddEvenForSingleNumber4()
+        {
+            OddEvenClass oddEven = new OddEvenClass();
+            int number = 4;
+            var actualresult = oddEven.PrintSingleOddEven(number);
+            var expectedresult = "Even";
+            Assert.AreEqual(expectedresult, actualresult);
+        }
+    }
+}
